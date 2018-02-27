cd105232453a71a1fed3db74f734ef2dd86a65fd
Bryan <>
2018-02-26 19:30:42 -0600

    Updated spotify script and module to change color when pausing

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -3,4 +3,9 @@
 artist=$(playerctl metadata xesam:artist)
 title=$(playerctl metadata xesam:title)
 
-echo $artist - $title
+if [ "$(playerctl status)" == "Paused" ]
+then
+    echo "%{F#f2cb90} $artist - $title%{F#88e9fd}"
+else
+    echo "$artist - $title"
+fi
